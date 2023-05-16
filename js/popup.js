7d18039713db9e1fa5b840ea28cadbfb362a1707
Denis <denis-levin@inbox.ru>
2023-05-16 20:34:01 +0300

    final

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,22 @@
-let loginButton = document.querySelector('.btn');
-let loginPopUp = document.querySelector('.login-popup');
+const loginButton = document.querySelector('.btn');
+const loginPopUp = document.querySelector('.login-popup');
+const closeButton = document.querySelector('.close');
 
 loginButton.addEventListener('click', showPopUp);
+closeButton.addEventListener('click', close);
 
 function showPopUp(evt) {
 evt.preventDefault();
 
 loginPopUp.classList.toggle('show-popup');
 };
+
+function close() {
+    loginPopUp.classList.remove('show-popup');
+}
+
+loginPopUp.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('show-pop')) {
+close();
+    }
+})
